a1f9f5566204f6dca9ec7ef1d5067e1c096203ca
Gabriel Venturini <dev.venturini@gmail.com>
2024-03-07 23:06:00 -0300

    day 2

---
+++ 1 Week Preparation/day 2/diagonal_difference.py
@@ -0,0 +1,40 @@
+#!/bin/python3
+import numpy as np
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'diagonalDifference' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY arr as parameter.
+#
+
+def diagonalDifference(arr):
+    # Write your code here
+    # HackerRank doesn't support Numpy 
+    # but I'm using this bcs that's how I solved it
+    diag1 = arr.diagonal().sum()
+    diag2 =  np.flipud(arr).diagonal().sum()
+
+    return abs(diag1 - diag2)
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = diagonalDifference(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
